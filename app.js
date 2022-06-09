cb3fd3aa23b033ebf8902dec28eecf59c5dff9ce
nowz <nowwebmaster73@gmail.com>
2022-06-10 02:57:10 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// puppeteer on WSL2
+
+const puppeteer = require('puppeteer');
+
+async function scrape() {
+   const browser = await puppeteer.launch({});
+   const page = await browser.newPage();
+   var fund = process.argv.slice(2);
+   let nav = "Fund does not exists.";
+   //first page
+   await page.goto('https://codequiz.azurewebsites.net/');
+   const selector = 'p' // 
+   var element = await page.waitForSelector("p");
+   var text = await page.evaluate(element => element.textContent, element);
+
+   //second page
+   await page.click('input[type="button"]');
+   var element = await page.waitForSelector("table");
+   var text = await page.evaluate(element => element.textContent, element);
+   var [element] = await page.$x(".//td[contains(., '" + fund + "')]");
+
+   //Found
+   if (element) {
+        nav = await page.evaluate(element => element.nextElementSibling.textContent, element);
+   }
+
+   console.log(nav);
+
+   browser.close();
+}
+scrape();
