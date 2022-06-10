157735397d24d8cf6009d6e49d10cb59680e37e4
nowz <nowwebmaster73@gmail.com>
2022-06-10 09:37:20 +0700

    added try-catch

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-// puppeteer on WSL2cd
+// puppeteer on WSL2
 
 const puppeteer = require('puppeteer');
 
 async function scrape() {
    const browser = await puppeteer.launch({});
    const page = await browser.newPage();
+   page.setDefaultTimeout(2000);
    var fund = process.argv.slice(2);
    let nav = "Fund does not exists.";
+
+
    //first page
-   await page.goto('https://codequiz.azurewebsites.net/');
+   try {
+      await page.goto('https://codequiz.azurewebsites.net/');
+   } catch (e) {
+        console.log('Could not connect to codequiz - ' + e);
+        process.exit();
+   }
+  
    const selector = 'p' // 
-   var element = await page.waitForSelector("p");
-   var text = await page.evaluate(element => element.textContent, element);
+
+   try{
+      var element = await page.waitForSelector("p");
+      var text = await page.evaluate(element => element.textContent, element);
+      await page.click('input[type="button"]');
+   }catch (e) {
+      console.log('The page might changed - ' + e);
+      process.exit();
+   }
+  
 
    //second page
-   await page.click('input[type="button"]');
-   var element = await page.waitForSelector("table");
-   var text = await page.evaluate(element => element.textContent, element);
-   var [element] = await page.$x(".//td[text()='" + fund + "']");
+   try{
 
-   //Found
-   if (element) {
-        nav = await page.evaluate(element => element.nextElementSibling.textContent, element);
+      var element = await page.waitForSelector("table");
+      var text = await page.evaluate(element => element.textContent, element);
+      var [element] = await page.$x(".//td[text()='" + fund + "']");
+
+      //Found
+      if (element) {
+         nav = await page.evaluate(element => element.nextElementSibling.textContent, element);
+      }
+
+      console.log(nav);
+
+   }catch (e) {
+      console.log('The page might changed - ' + e);
+      process.exit();
    }
-
-   console.log(nav);
 
    browser.close();
 }
